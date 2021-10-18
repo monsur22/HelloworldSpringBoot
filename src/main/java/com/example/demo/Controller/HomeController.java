c6b3f877e699e542d876df4f3d3137d081912e44
monsur22 <monsurahmedshafiq@gmail.com>
2021-10-18 20:37:45 +0600

    update pom.xml

--- src/main/java/com/example/demo/Controller/HomeController.java
+++ src/main/java/com/example/demo/Controller/HomeController.java
@@ -5,8 +5,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    @GetMapping("/")
+    @GetMapping("/home")
     public String index(){
-        return "index";
+        return "home";
+    }
+    @GetMapping("/about")
+    public String about(){
+        return "about";
+    }
+    @GetMapping("/contact")
+    public String contact(){
+        return "contact";
+    }
+    @GetMapping("/")
+    public String layout(){
+        return "layout";
     }
 }
